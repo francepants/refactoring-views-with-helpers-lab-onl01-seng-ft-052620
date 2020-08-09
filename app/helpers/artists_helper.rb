550f7a95d4a8c5161bc765ed9aeb9b7787df3b73
francepants <franceline90@gmail.com>
2020-08-09 17:41:10 -0500

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -4,17 +4,17 @@ module ArtistsHelper
     # displays a link to edit the song if artist empty
     # displays a link to the artist page if artist not empty
  
-    # def display_artist(song)
-    #     artist = song.artist
-    #     artist ? link_to(artist.name, artist_path(artist)) : link_to("Add Artist", edit_song_path(song))
-    # end
-
-
     def display_artist(song)
-        if song.artist.present?
-        link_to song.artist.name, artist_path(song.artist)
-        else
-        link_to "Add Artist", edit_song_path(song)
-      end
+        artist = song.artist
+        artist ? link_to(artist.name, artist_path(artist)) : link_to("Add Artist", edit_song_path(song))
     end
+
+
+    # def display_artist(song)
+    #     if song.artist.present?
+    #     link_to song.artist.name, artist_path(song.artist)
+    #     else
+    #     link_to "Add Artist", edit_song_path(song)
+    #   end
+    # end
 end
